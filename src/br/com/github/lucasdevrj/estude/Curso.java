4a05ef444030759199b3de13acf0499255f53b5b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-05 18:32:59 -0300

    Criação de todos os atributos da classe Curso.

--- src/br/com/github/lucasdevrj/estude/Curso.java
+++ src/br/com/github/lucasdevrj/estude/Curso.java
@@ -7,5 +7,8 @@ public class Curso {
 	private String nome;
 	private String codigo;
 	private Date tempoHoras;
-	
+	private String publicoAlvo;
+	private String instrutor;
+	private String ementa;
+	private String habilidadesDesenvolvidas;
 }
--- src/br/com/github/lucasdevrj/estude/Visibilidade.java
+++ src/br/com/github/lucasdevrj/estude/Visibilidade.java
@@ -2,4 +2,5 @@ package br.com.github.lucasdevrj.estude;
 
 public enum Visibilidade {
 
+	PRIVADO, PUBLICO;
 }
