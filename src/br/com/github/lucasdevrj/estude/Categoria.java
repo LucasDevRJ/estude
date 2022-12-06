f87dbc837254379f19dcf856811fbec15311bf1d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-06 09:37:41 -0300

    Criação dos Getters e Setters da classe Categoria;

--- src/br/com/github/lucasdevrj/estude/Categoria.java
+++ src/br/com/github/lucasdevrj/estude/Categoria.java
@@ -7,6 +7,71 @@ public class Categoria {
 	private String descricao;
 	private String guia;
 	private Indicacao indicacao;
+	private Integer ordem;
 	private String urlImagem;
 	private String hexadecimal;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getGuia() {
+		return guia;
+	}
+	
+	public void setGuia(String guia) {
+		this.guia = guia;
+	}
+	
+	public Indicacao getIndicacao() {
+		return indicacao;
+	}
+	
+	public void setIndicacao(Indicacao indicacao) {
+		this.indicacao = indicacao;
+	}
+	
+	public Integer getOrdem() {
+		return ordem;
+	}
+	
+	public void setOrdem(Integer ordem) {
+		this.ordem = ordem;
+	}
+	
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	
+	public String getHexadecimal() {
+		return hexadecimal;
+	}
+	
+	public void setHexadecimal(String hexadecimal) {
+		this.hexadecimal = hexadecimal;
+	}
 }
