b1bd1e6fffd7ea3eed55e7c0178cc1a0a1d12743
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-06 19:37:31 -0300

    Criação dos métodos Getters e Setter da classe Alternativa.

--- src/br/com/github/lucasdevrj/estude/Alternativa.java
+++ src/br/com/github/lucasdevrj/estude/Alternativa.java
@@ -7,4 +7,44 @@ public class Alternativa {
 	private String indicacao;
 	private String justificativa;
 	private Questao questao;
+	
+	public String getTexto() {
+		return texto;
+	}
+	
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	public int getOrdem() {
+		return ordem;
+	}
+	
+	public void setOrdem(int ordem) {
+		this.ordem = ordem;
+	}
+	
+	public String getIndicacao() {
+		return indicacao;
+	}
+	
+	public void setIndicacao(String indicacao) {
+		this.indicacao = indicacao;
+	}
+	
+	public String getJustificativa() {
+		return justificativa;
+	}
+	
+	public void setJustificativa(String justificativa) {
+		this.justificativa = justificativa;
+	}
+	
+	public Questao getQuestao() {
+		return questao;
+	}
+	
+	public void setQuestao(Questao questao) {
+		this.questao = questao;
+	}
 }
