4e90e8d3f89240b5b68518444b47d2ad5180320f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-06 19:33:00 -0300

    Criação da Classe Alternativa com seus atributos.

---
+++ src/br/com/github/lucasdevrj/estude/Alternativa.java
@@ -0,0 +1,10 @@
+package br.com.github.lucasdevrj.estude;
+
+public class Alternativa {
+
+	private String texto;
+	private int ordem;
+	private String indicacao;
+	private String justificativa;
+	private Questao questao;
+}
