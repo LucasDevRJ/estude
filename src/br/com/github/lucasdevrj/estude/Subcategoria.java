13b5c1ef60462b82d3ca02314bd15e030399b9f8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-06 19:16:35 -0300

    Criação da classe Subcategoria com seus atributos.

---
+++ src/br/com/github/lucasdevrj/estude/Subcategoria.java
@@ -0,0 +1,12 @@
+package br.com.github.lucasdevrj.estude;
+
+public class Subcategoria {
+
+	private String nome;
+	private String codigo;
+	private String descricao;
+	private String guia;
+	private String indicacao;
+	private int ordem;
+	private Categoria categoria;
+}
