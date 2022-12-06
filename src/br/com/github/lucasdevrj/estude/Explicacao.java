84cdc8b6e39e542c4f57f3c5c96d688fc09f1cb6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-06 12:37:16 -0300

    Criação da classe Explicacao, com seus atributos e métodos.

---
+++ src/br/com/github/lucasdevrj/estude/Explicacao.java
@@ -0,0 +1,18 @@
+package br.com.github.lucasdevrj.estude;
+
+public class Explicacao {
+
+	private String explicacao;
+	
+	public Explicacao(String explicacao) {
+		this.explicacao = explicacao;
+	}
+	
+	public String getExplicacao() {
+		return explicacao;
+	}
+	
+	public void setExplicacao(String explicacao) {
+		this.explicacao = explicacao;
+	}
+}
