3a6e7e1530ec7044611d75af1dfadc618ed7b2ac
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-07 07:22:41 -0300

    Criação dos métodos Getters e Setters na classe Questao.

--- src/br/com/github/lucasdevrj/estude/Secao.java
+++ src/br/com/github/lucasdevrj/estude/Secao.java
@@ -8,4 +8,52 @@ public class Secao {
 	private String indicacaoAtiva = "inativa";
 	private String indicacaoProva = "não";
 	private Curso curso;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public int getOrdem() {
+		return ordem;
+	}
+	
+	public void setOrdem(int ordem) {
+		this.ordem = ordem;
+	}
+	
+	public String getIndicacaoAtiva() {
+		return indicacaoAtiva;
+	}
+	
+	public void setIndicacaoAtiva(String indicacaoAtiva) {
+		this.indicacaoAtiva = indicacaoAtiva;
+	}
+	
+	public String getIndicacaoProva() {
+		return indicacaoProva;
+	}
+	
+	public void setIndicacaoProva(String indicacaoProva) {
+		this.indicacaoProva = indicacaoProva;
+	}
+	
+	public Curso getCurso() {
+		return curso;
+	}
+	
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
 }
