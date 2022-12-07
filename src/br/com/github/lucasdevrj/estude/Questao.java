bd3fe1950eaf6fb8c3abd558461ea59f85af25c1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-07 07:16:07 -0300

    Criação da classe Questao com seus atributos.

--- src/br/com/github/lucasdevrj/estude/Questao.java
+++ src/br/com/github/lucasdevrj/estude/Questao.java
@@ -2,4 +2,22 @@ package br.com.github.lucasdevrj.estude;
 
 public class Questao {
 
+	private String enunciado;
+	private String tipo = "Resposta unica";
+	
+	public String getEnunciado() {
+		return enunciado;
+	}
+	
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 }
