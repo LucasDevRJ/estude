f64589a2914caca0e91c17c7cf3f72ef87e7cf0f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-07 07:21:35 -0300

    Criação dos atributos da classe Questao.

--- src/br/com/github/lucasdevrj/estude/Secao.java
+++ src/br/com/github/lucasdevrj/estude/Secao.java
@@ -2,4 +2,10 @@ package br.com.github.lucasdevrj.estude;
 
 public class Secao {
 
+	private String nome;
+	private String codigo;
+	private int ordem;
+	private String indicacaoAtiva = "inativa";
+	private String indicacaoProva = "não";
+	private Curso curso;
 }
