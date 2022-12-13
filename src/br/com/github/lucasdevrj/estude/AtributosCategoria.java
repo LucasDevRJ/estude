9aa78822bb285f015616f35c2903574e20d3f1ee
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-13 08:27:57 -0300

    Criação da classe de Atributos da Categoria, com seus métodos e construtor.

--- src/br/com/github/lucasdevrj/estude/AtributosCategoria.java
+++ src/br/com/github/lucasdevrj/estude/AtributosCategoria.java
@@ -1,16 +1,14 @@
 package br.com.github.lucasdevrj.estude;
 
-public class Subcategoria {
+public abstract class AtributosCategoria {
 
 	private String nome;
 	private String codigo;
 	private String descricao;
 	private String guia;
-	private String indicacao;
 	private int ordem;
-	private Categoria categoria;
 	
-	public Subcategoria(String nome, String codigo) {
+	public AtributosCategoria(String nome, String codigo) {
 		Validacao.validaDados(nome, codigo);
 		this.nome = nome;
 		this.codigo = codigo;
@@ -22,17 +20,5 @@ public class Subcategoria {
 	
 	public void setGuia(String guia) {
 		this.guia = guia;
-	}
-	
-	public void setIndicacao(String indicacao) {
-		this.indicacao = indicacao;
-	}
-	
-	public void setOrdem(int ordem) {
-		this.ordem = ordem;
-	}
-	
-	public void setCategoria(Categoria categoria) {
-		this.categoria = categoria;
 	}
 }
--- src/br/com/github/lucasdevrj/estude/Categoria.java
+++ src/br/com/github/lucasdevrj/estude/Categoria.java
@@ -2,28 +2,12 @@ package br.com.github.lucasdevrj.estude;
 
 public class Categoria {
 
-	private String nome;
-	private String codigo;
-	private String descricao;
-	private String guia;
 	private boolean indicacaoAtiva = false;
 	private int ordem;
 	private String urlImagem;
 	private String hexadecimal;
 	
-	public Categoria(String nome, String codigo) {
-		Validacao.validaDados(nome, codigo);
-		this.nome = nome;
-		this.codigo = codigo;
-	}
 	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	
-	public void setGuia(String guia) {
-		this.guia = guia;
-	}
 	
 	public void setIndicacaoAtiva(boolean indicacaoAtiva) {
 		this.indicacaoAtiva = indicacaoAtiva;
