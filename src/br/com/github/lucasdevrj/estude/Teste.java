b570b9250cc298e44737be40ab5a866efbb1fbc2
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-01-07 16:13:14 -0300

    Testes de código.

--- src/br/com/github/lucasdevrj/estude/Teste.java
+++ src/br/com/github/lucasdevrj/estude/Teste.java
@@ -4,6 +4,15 @@ public class Teste {
 
 	public static void main(String[] args) {
 		//Todos os erros já foram testados
-		Curso curso = new Curso("Java OO", "afrg34ff", 10, "");
+//		Curso curso = new Curso("Java OO", "afrg34ff", 10, "");
+		
+		Questao questao = new Questao("dnfdjfdf");
+		Explicacao explicacao = new Explicacao("dlskçfkf");
+		Video video = new Video("dfdfdfddfdd");
+		Atividade atividade = new Atividade("Java SE", "dfgdfdf334", null);
+		atividade.setTipo(questao);
+
+		
+		System.out.println(atividade);
 	}
 }
